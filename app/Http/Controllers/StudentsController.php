4d2975f7bca8ea2dc31dd69a8949600865cadee9
arun raj <arunraj99675@gmail.com>
2024-06-02 12:52:05 +0530

    mail function

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -27,5 +27,16 @@ class StudentsController extends Controller
       );
      Students::create($data);
      echo 'data saved successfully';
+     Mail::send('Html.view', $data, function ($message) {
+         $message->from('john@johndoe.com', 'John Doe');
+         $message->sender('john@johndoe.com', 'John Doe');
+         $message->to('john@johndoe.com', 'John Doe');
+         $message->cc('john@johndoe.com', 'John Doe');
+         $message->bcc('john@johndoe.com', 'John Doe');
+         $message->replyTo('john@johndoe.com', 'John Doe');
+         $message->subject('Subject');
+         $message->priority(3);
+         $message->attach('pathToFile');
+     });
     }
 }
