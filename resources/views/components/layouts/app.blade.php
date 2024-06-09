4e883eb39b3c149156ab82b9c66505d3b92b50ed
arun raj <arunraj99675@gmail.com>
2024-06-09 12:27:05 +0530

    Added new features

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Pages Title' }}</title>
+        <title>{{ $title ?? 'Pagesg Title' }}</title>
     </head>
     <body>
         {{ $slot }}
